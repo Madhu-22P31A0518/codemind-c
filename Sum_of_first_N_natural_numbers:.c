20aa690dda52a168460f56dded9902c98ee145b8
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-07 21:52:56 +0530

    #include<stdio.h> int sum(int n) {     int a;     a=(n*(n+1))/2;     return a; } int main() {     int n;     scanf("%d",&n);     int x;     x=sum(n);     printf("%d",x); }

---
+++ Sum_of_first_N_natural_numbers:.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sum(int n)
+{
+    int a;
+    a=(n*(n+1))/2;
+    return a;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int x;
+    x=sum(n);
+    printf("%d",x);
+}
