d0aa8704f66126122b8954bd77da6bbc515fc24f
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-14 21:27:48 +0530

    #include<stdio.h> void di(int x,int y,int z) {     int c;     c=(5*x+10*y)/z;     printf("%d",c); } int main() {    int x,y,z;    scanf("%d%d%d",&x,&y,&z);    di(x,y,z); }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+void di(int x,int y,int z)
+{
+    int c;
+    c=(5*x+10*y)/z;
+    printf("%d",c);
+}
 int main()
 {
-    int x,y,z;
-    scanf("%d%d%d",&x,&y,&z);
-    int n;
-    n=(5*x+10*y)/z;
-    printf("%d",n);
+   int x,y,z;
+   scanf("%d%d%d",&x,&y,&z);
+   di(x,y,z);
 }
