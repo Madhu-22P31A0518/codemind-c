f1f0d896e02c060c635765b7aecda777e7892556
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-16 21:53:50 +0530

    #include<stdio.h> int pr(int a,int b) {     if(b>a)     {         return 0;     } } int main() {     int a,b;     scanf("%d%d",&a,&b);     int x=pr(a,b);     if(x==0)     {         printf("Profit");     }     else     {         printf("Loss");     } }

--- Profit.c
+++ Profit.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+int pr(int a,int b)
+{
+    if(b>a)
+    {
+        return 0;
+    }
+}
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    if(b>a)
+    int x=pr(a,b);
+    if(x==0)
     {
         printf("Profit");
     }
