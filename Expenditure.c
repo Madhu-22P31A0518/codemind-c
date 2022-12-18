3b43d1bc9fe3f57bb1fa3d506ce4836156a51cd4
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-18 18:44:49 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a>=b*30)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Expenditure.c
+++ Expenditure.c
@@ -1,19 +1,9 @@
 #include<stdio.h>
-int ex(int a,int b)
-{
-    if(a>=b*30)
-    {
-        return 0;
-    }
-   
-}
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    int x;
-    x=ex(a,b);
-    if(x==0)
+    if(a>=b*30)
     {
         printf("YES");
     }
