eb7a7ba47eb09ce5876d77ec8e0f03df938c611b
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-21 21:20:03 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int h,m;     h=a/365;     m=(a%365)/7;     printf("%d %d",h,m); }

--- Days_into_years,_weeks:.c
+++ Days_into_years,_weeks:.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
-#include<math.h>
-int ds(int a)
-{
-   int d,y;
-   y=a/365;
-   d=(a%365)/7;
-   printf("%d
-%d",y,d);
-}
 int main()
 {
     int a;
     scanf("%d",&a);
-    ds(a);
+    int h,m;
+    h=a/365;
+    m=(a%365)/7;
+    printf("%d
+%d",h,m);
 }
