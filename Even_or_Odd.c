2658232de08640391fc1952cd60e6609ba55e985
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2022-12-23 21:26:42 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%2==0)     {         printf("Even");     }     else     {         printf("Odd");     } }

--- Even_or_Odd.c
+++ Even_or_Odd.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
-int eo(int a)
-{
-    if(a%2==0)
-    {
-        return 0;
-    }
-}
 int main()
 {
     int a;
     scanf("%d",&a);
-    int x=eo(a);
-    if(x==0)
+    if(a%2==0)
     {
         printf("Even");
     }
